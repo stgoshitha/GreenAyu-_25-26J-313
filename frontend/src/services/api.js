7a7cf653ec888e80173f4a27ffe2b0ab9f764a8a
stgoshitha <wkgoshithapremachandra@gmail.com>
2026-05-05 12:45:23 +0530

    update base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://lure-clicker-purging.ngrok-free.dev',
 });
 
 // Add a request interceptor to include JWT token
